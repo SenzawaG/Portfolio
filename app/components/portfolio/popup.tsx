fea2e44c701f321e32cbc4334b032383d02b112d
SalamPS <salamp@salamp.id>
2024-12-24 05:59:07 +0700

    feat(all): Updated content

--- app/components/portfolio/popup.tsx
+++ app/components/portfolio/popup.tsx
@@ -22,10 +22,10 @@ export default function PopupPortfolio ({popup, setPopup, mode}: {popup:number, 
 		<div className={`backdrop-blur-sm bg-opacity-50 fixed inset-0 h-full w-full duration-500 bg-black ${popup === -1 ? ' opacity-0 z-[-1]' : 'opacity-100 z-[997]'}`}></div>
 		<div className={`z-[999] px-2 md:px-0 fixed inset-0 h-[100svh] flex justify-center items-center duration-500 ${popup === -1 ? 'translate-y-[-100svh]' : ''}`}>
 			<div className="fixed inset-0 h-full w-full" onClick={() => {setPopup(-1)}}></div>
-			<div className="z-[998] w-full md:w-[40%] bg-lampblack px-4 py-6 md:p-8 rounded-2xl">
+			<div className="z-[998] w-full md:w-[60%] lg:w-[50%] bg-lampblack px-4 py-6 md:p-8 rounded-2xl">
 				<h1 className="text-xl md:text-2xl pb-3 md:pb-4">
 					<span className="border-b-4 pb-1 border-gray-400 inline-block px-2">
-						{item?.name}
+						{item?.name} {item?.year ? `- ${item.year}` : ''}
 					</span>
 				</h1>
 				<p className="text-gray-200 text-xs md:text-base">
@@ -34,8 +34,8 @@ export default function PopupPortfolio ({popup, setPopup, mode}: {popup:number, 
 				<div className="grid grid-cols-2 gap-2 my-2 md:my-3">
 					{item?.imageUrl 
 						&& item.imageUrl.map((img, i) => <>
-							<div key={item?.name+img+i} 
-								className="rounded-md aspect-video bg-cover bg-no-repeat my-2 md:my-3" 
+							<div key={img+i} 
+								className={`rounded-md aspect-video bg-cover bg-no-repeat my-2 md:my-3 ${item?.imageUrl && item.imageUrl.length < 2 ? 'col-span-2' : ''}`}	 
 								style={{backgroundImage: `url(/assets/portfolio-ev/${img})`, backgroundPosition: 'center top'}}>
 							</div>
 						</>)
